0eaa372dd753c0cd0c867674e60de9f58b17586a
Micah Parks <git@micahparks.com>
2020-12-17 20:42:01 -0500

    More fun search && results.

--- main.go
+++ main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"index/suffixarray"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"strings"
+
+	"github.com/sahilm/fuzzy"
 )
 
 func main() {
@@ -18,27 +17,27 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	searcher.Search("Hamlet")
 
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
-
-	http.HandleFunc("/search", handleSearch(searcher))
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3001"
-	}
-
-	fmt.Printf("Listening on port %s...", port)
-	err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//fs := http.FileServer(http.Dir("./static"))
+	//http.Handle("/", fs)
+	//
+	//http.HandleFunc("/search", handleSearch(searcher))
+	//
+	//port := os.Getenv("PORT")
+	//if port == "" {
+	//	port = "3001"
+	//}
+	//
+	//fmt.Printf("Listening on port %s...", port)
+	//err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 }
 
 type Searcher struct {
 	CompleteWorks []string
-	SuffixArray   *suffixarray.Index
 }
 
 func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
@@ -63,34 +62,55 @@ func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request
 	}
 }
 
-func (s *Searcher) Load(filename string) error {
-	fileData, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return fmt.Errorf("Load: %w", err)
+// Load loads the give file as a slice of string with trimmed space.
+func (s *Searcher) Load(filePath string) (err error) {
+
+	// Read the file into memory.
+	var fileData []byte
+	if fileData, err = ioutil.ReadFile(filePath); err != nil {
+		return err
 	}
 
 	// Split the file data by newlines.
 	split := strings.Split(string(fileData), "\n")
 
-	// Create a slice to store the file data in the same length as the number of elements in the slice, to save on
-	// memory.
-	s.CompleteWorks = make([]string, len(split))
+	// Create a slice to store the file data.
+	s.CompleteWorks = make([]string, 0)
 
-	// Trim the space for the line and add it to the resulting slice.
-	for index, line := range split {
-		s.CompleteWorks[index] = strings.TrimSpace(line)
+	// Trim the space for the line and add it to the resulting slice. Do not add empty strings.
+	for _, line := range split {
+		if line = strings.TrimSpace(line); line != "" {
+			s.CompleteWorks = append(s.CompleteWorks, line) // TODO Only add unique strings?
+		}
 	}
 
-	s.CompleteWorks = string(fileData)
-	s.SuffixArray = suffixarray.New(fileData)
 	return nil
 }
 
-func (s *Searcher) Search(query string) []string {
-	idxs := s.SuffixArray.Lookup([]byte(query), -1)
-	results := []string{}
-	for _, idx := range idxs {
-		results = append(results, s.CompleteWorks[idx-250:idx+250])
+func (s *Searcher) Search(query string) (results []string) {
+
+	// Find all matching strings. Make that external dependency do all the work.
+	matches := fuzzy.Find(query, s.CompleteWorks)
+
+	// Create a map to behave as set of strings. The value is the original index.
+	strSet := make(map[string]uint)
+
+	// Add each match to the set of strings only once. Keep track of the indexes.
+	index := uint(0)
+	for _, match := range matches {
+		if _, ok := strSet[match.Str]; !ok {
+			strSet[match.Str] = index
+			index++
+		}
 	}
+
+	// Allocate the required memory for the return slice so it's faster to add all the strings.
+	results = make([]string, len(strSet))
+
+	// Turn the set of strings back into a slice. Insert the matches to their proper index.
+	for str, index := range strSet {
+		results[index] = str
+	}
+
 	return results
 }
