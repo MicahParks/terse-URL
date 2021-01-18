eeca9fd924dd330f06c8a8c69e95e02d474e9661
Micah Parks <git@micahparks.com>
2021-01-17 20:06:11 -0500

    Adding TODO.

--- frontend/prefix.js
+++ frontend/prefix.js
@@ -1,4 +1,4 @@
-async function getPrefix() {
+async function getPrefix() { // TODO Use local storage for this?
     let response = await fetch(`/api/prefix`);
     response.json().then(prefix => {
         document.getElementById("httpPrefix").textContent = prefix;
