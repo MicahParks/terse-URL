14beba679a6554d611bbfd245a17b829736b7c49
Micah Parks <git@micahparks.com>
2021-01-24 16:39:08 -0500

    Summarize.

--- frontend/summary.js
+++ frontend/summary.js
@@ -1,13 +1,13 @@
-function SummaryInit(summary) {
-    this.body = JSON.stringify(summary);
+function SummaryInit(shortenedURLs) {
+    this.body = JSON.stringify(shortenedURLs);
     this.headers = {
         "Content-Type": "application/json"
     };
     this.method = "POST";
 }
 
-async function summaryAll(summary) {
-    return fetch("/api/summary", new SummaryInit(summary))
+async function summarize(shortenedURLs) {
+    return fetch("/api/summary", new SummaryInit(shortenedURLs))
         .then(function (response) {
             return response.json();
         })
