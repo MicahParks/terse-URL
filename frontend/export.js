1b0ea65be9d5b08f7b7298fdb364eb1fe4345f7c
Micah Parks <git@micahparks.com>
2021-02-14 15:13:21 -0500

    Downloading exports.

--- frontend/export.js
+++ frontend/export.js
@@ -1,3 +1,11 @@
+function ExportInit(shortenedURLs) {
+    this.body = JSON.stringify(shortenedURLs);
+    this.headers = {
+        "Content-Type": "application/json"
+    };
+    this.method = "POST";
+}
+
 async function exportAll() {
     return fetch("/api/export")
         .then(function (response) {
@@ -5,8 +13,8 @@ async function exportAll() {
         })
 }
 
-async function exportOne(shortened) {
-    return fetch(`/api/export/${shortened}`)
+async function exportSome(shortenedURLs) {
+    return fetch(`/api/export/some`, new ExportInit(shortenedURLs))
         .then(function (response) {
             return response.json();
         });
