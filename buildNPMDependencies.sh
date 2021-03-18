d02365b1693bcb0390d5ea09f6498210b8d99953
Micah Parks <git@micahparks.com>
2021-03-18 15:08:43 -0400

    Using CDN for swaggerjs.

--- buildNPMDependencies.sh
+++
@@ -1,5 +0,0 @@
-# Build the dependencies code.
-npx webpack
-
-# Move the precompiled code to where it can be hosted.
-mv dist/main.js frontend/
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import SwaggerClient from 'swagger-client';
-
-// TODO This is a cheap and easy way to get access to the dependency. Doesn't seem to be the right way...
-window.SwaggerClient = SwaggerClient;
